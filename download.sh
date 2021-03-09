9c7c40d5d45fc0c41e221e913b0de49bce9ccf7c
Casey Schneider-Mizell <caseysm@gmail.com>
2021-03-08 17:04:16 -0800

    Update download.sh for mesh+synapse data

--- download.sh
+++ download.sh
@@ -2,12 +2,9 @@ DATADIR=data
 mkdir -p ${DATADIR}
 cd $DATADIR
 
-#MeshExample
-wget -q -O soma_subgraph_synapses_spines_v185.csv "https://zenodo.org/record/3710459/files/soma_subgraph_synapses_spines_v185.csv?download=1"
-
-
-# multiscale/mitochondria
-gdown --id 1OIUpHr9gVYKVagDqpom9Sx9qb5iAX5Km -O multiscale_mito_intermediate_data.tgz \
-&& tar -xzf multiscale_mito_intermediate_data.tgz 
+# Download larger data files
+wget -O pni_synapses_v185.csv "https://zenodo.org/record/3710459/files/pni_synapses_v185.csv" --show-progress
+wget -O 648518346349538192.h5 "https://ndownloader.figshare.com/files/26736710" --show-progress
+wget -O 648518346349539856.h5 "https://ndownloader.figshare.com/files/26736767" --show-progress
 
 cd ..
